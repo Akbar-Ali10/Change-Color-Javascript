af6f401a30ff613b223daba6859424f0d4b2e240
Akbar-Ali <poppykate28@gmail.com>
2024-09-30 17:52:55 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+
+
+
+
+let blue = document.getElementById("blue").addEventListener("click", function(){
+    document.querySelector("body").style.backgroundColor = "blue"
+})
+
+function green()
+{ document.querySelector("body").style.backgroundColor = "green"}
+
+let yellow = document.getElementById("yellow").addEventListener("click", function(){
+    document.querySelector("body").style.backgroundColor ="yellow"
+})
+
+function aqua(){
+    document.querySelector("body").style.backgroundColor ="aqua"
+}
+
+let black = document.getElementById("brown").addEventListener("click", function(){
+    document.querySelector("body").style.backgroundColor ="brown"
+})
+
+function darkgoldenrod(){
+    document.querySelector("body").style.backgroundColor ="darkgoldenrod"
+}
+
+let gold = document.getElementById  ("gold").addEventListener("click", function(){
+    document.querySelector("body").style.backgroundColor = "gold"
+})
+
+function greenyellow(){
+    document.querySelector("body").style.backgroundColor ="greenyellow"
+}
+
+let navy = document.getElementById  ("navy").addEventListener("click", function(){
+    document.querySelector("body").style.backgroundColor = "navy"
+})
+
+function wheat(){
+    document.querySelector("body").style.backgroundColor = "wheat"
+}
